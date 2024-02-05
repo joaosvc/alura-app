6c6ade9a6b8afcd9bbf42745ea72642bd6c8c92b
Joaosvc <joaosvc.dev@gmail.com>
2024-02-05 09:53:31 -0300

    feat: Setup dashboard

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,3 +1,15 @@
+"use client";
+
+import DashboardPage from "@/components/dashboard/page";
+import LoadingPage from "@/components/loading/loading-page";
+import { useSession } from "next-auth/react";
+
 export default function Dashboard() {
-  return <>This is Dashboard</>;
+  const { data: session, status } = useSession({ required: true });
+
+  if (status === "loading") {
+    return <LoadingPage />;
+  }
+
+  return <DashboardPage user={session.user} />;
 }

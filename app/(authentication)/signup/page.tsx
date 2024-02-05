d57b173747984f39e81b3e55038cd623cee74ca6
Joaosvc <joaosvc.dev@gmail.com>
2024-02-05 10:37:38 -0300

    feat: Fixing Auth Layout

--- app/(authentication)/signup/page.tsx
+++ app/(authentication)/signup/page.tsx
@@ -4,7 +4,7 @@ export default function Signup() {
   return (
     <section className="min-h-screen bg-white dark:bg-gray-900">
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0 pt-48">
-        <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+        <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
             <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
               Criar uma conta

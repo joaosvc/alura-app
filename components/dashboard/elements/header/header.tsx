220505099185d329f0d3d19f902421b7e3798255
Joaosvc <joaosvc.dev@gmail.com>
2024-02-06 09:54:52 -0300

    feat: Adding link of dashboard to profile header

--- components/dashboard/elements/header/header.tsx
+++ components/dashboard/elements/header/header.tsx
@@ -3,13 +3,16 @@
 import { signOut } from "next-auth/react";
 import { User } from "@/client/structs/types/next-auth";
 import Profile from "./profile";
+import Link from "next/link";
 
 export default function Header({ user }: { user: User }) {
   const handleLogout = async () => await signOut();
 
   return (
     <header className="flex flex-row justify-between items-center p-6 px-12 lg:px-8 mx-auto max-w-7xl">
-      <Profile user={user} type="dashboard" />
+      <Link href="/">
+        <Profile user={user} type="dashboard" />
+      </Link>
 
       <div className="cursor-pointer hover:underline" onClick={handleLogout}>
         {"Log out "}

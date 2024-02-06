37ef6eae7198e2eea89e5153094083a6a167eb76
Joaosvc <joaosvc.dev@gmail.com>
2024-02-06 00:54:30 -0300

    feat: removing bottom margin of categories card

--- components/dashboard/elements/categories/category/card.tsx
+++ components/dashboard/elements/categories/category/card.tsx
@@ -14,7 +14,7 @@ export default function CategoryCard({
   return (
     <div className="w-1/2 lg:w-1/4 p-2" {...props}>
       <div className="p-3 lg:p-6 pb-1 lg:pb-2 h-full hover:bg-gray-100 dark:hover:bg-gray-800 rounded cursor-pointer">
-        <h2 className="text-lg font-bold mb-1">{category}</h2>
+        <h2 className="text-lg font-bold">{category}</h2>
 
         <div className="h-14 lg:h-16 w-full text-sm lg:text-base text-gray-600 overflow-hidden">
           <p className="line-clamp-2">{previewModules.join(", ")}</p>

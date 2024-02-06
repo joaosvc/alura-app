cba62d30de6c750ee5bbc1f53998b1de21400628
Joaosvc <joaosvc.dev@gmail.com>
2024-02-06 02:53:04 -0300

    feat: Adding session to AuthProvider

--- components/auth/provider/provider.tsx
+++ components/auth/provider/provider.tsx
@@ -1,11 +1,13 @@
 "use client";
 
+import { Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 
 interface AuthProviderProps {
   children: React.ReactNode;
+  session: Session;
 }
 
-export default function AuthProvider({ children }: AuthProviderProps) {
-  return <SessionProvider>{children}</SessionProvider>;
+export default function AuthProvider({ children, session }: AuthProviderProps) {
+  return <SessionProvider session={session}>{children}</SessionProvider>;
 }

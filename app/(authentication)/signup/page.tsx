0dc53120f219224a36db1ad2b913ecd78611c20a
Joaosvc <joaosvc.dev@gmail.com>
2024-02-08 21:12:10 -0300

    fix: Fixing auth layout

--- app/(authentication)/signup/page.tsx
+++ app/(authentication)/signup/page.tsx
@@ -3,10 +3,10 @@ import SignupForm from "@/components/auth/form/signup";
 export default function Signup() {
   return (
     <section className="min-h-screen bg-white dark:bg-gray-900">
-      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0 pt-48">
-        <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+      <div className="flex flex-col items-center justify-center px-6 mx-auto h-screen">
+        <div className="w-full rounded-lg shadow dark:border max-w-md bg-white dark:bg-gray-800 dark:border-gray-700">
+          <div className="p-6 space-y-4 sm:space-y-6 sm:p-8">
+            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 sm:text-2xl dark:text-white">
               Criar uma conta
             </h1>
 

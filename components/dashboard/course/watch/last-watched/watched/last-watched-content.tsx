de0142f2d65b8a1c4717a359e468926f893776ce
Joaosvc <joaosvc.dev@gmail.com>
2024-02-22 23:06:36 -0300

    feat: Adding last videos validation

--- components/dashboard/course/watch/last-watched/watched/last-watched-content.tsx
+++ components/dashboard/course/watch/last-watched/watched/last-watched-content.tsx
@@ -43,44 +43,6 @@ export default function LastWatchedContent({
     }
   }, [loading]);
 
-  const Navigation = () => {
-    return (
-      <>
-        {showLeftButton && (
-          <div className="absolute top-0 left-0 h-full flex items-center bg-gradient-to-r from-gray-200 dark:from-gray-800 to-transparent">
-            <button
-              onClick={() => {
-                watchContainer.current?.scrollBy({
-                  left: -watchContainer.current?.offsetWidth,
-                  behavior: "smooth",
-                });
-              }}
-              className="px-4 h-full flex items-center justify-center text-gray-500 dark:text-gray-300 hover:text-gray-700 dark:hover:text-white focus:outline-none rounded-l-full"
-            >
-              <FontAwesomeIcon icon={faChevronLeft} />
-            </button>
-          </div>
-        )}
-
-        {showRightButton && (
-          <div className="absolute top-0 right-0 h-full flex items-center bg-gradient-to-l from-gray-200 dark:from-gray-800 to-transparent">
-            <button
-              onClick={() => {
-                watchContainer.current?.scrollBy({
-                  left: watchContainer.current?.offsetWidth,
-                  behavior: "smooth",
-                });
-              }}
-              className="px-4 h-full flex items-center justify-center text-gray-500 dark:text-gray-300 hover:text-gray-700 dark:hover:text-white focus:outline-none rounded-l-full"
-            >
-              <FontAwesomeIcon icon={faChevronRight} />
-            </button>
-          </div>
-        )}
-      </>
-    );
-  };
-
   return (
     <>
       <div className="flex justify-between items-center">
@@ -88,14 +50,53 @@ export default function LastWatchedContent({
       </div>
 
       <div className="relative mt-4 w-full">
-        <div
-          className="grid grid-flow-col w-full h-full overflow-x-auto md:overflow-hidden overscroll-x-auto space-x-2 justify-start hide-scrollbar"
-          ref={watchContainer}
-        >
-          <LastWatchedItems user={user} items={lastWatched} />
-        </div>
-
-        <Navigation />
+        {lastWatched.length === 0 ? (
+          <>
+            {" "}
+            <div
+              className="grid grid-flow-col w-full h-full overflow-x-auto md:overflow-hidden overscroll-x-auto space-x-2 justify-start hide-scrollbar"
+              ref={watchContainer}
+            >
+              <LastWatchedItems user={user} items={lastWatched} />
+            </div>
+            {showLeftButton && (
+              <div className="absolute top-0 left-0 h-full flex items-center bg-gradient-to-r from-gray-200 dark:from-gray-800 to-transparent">
+                <button
+                  onClick={() => {
+                    watchContainer.current?.scrollBy({
+                      left: -watchContainer.current?.offsetWidth,
+                      behavior: "smooth",
+                    });
+                  }}
+                  className="px-4 h-full flex items-center justify-center text-gray-500 dark:text-gray-300 hover:text-gray-700 dark:hover:text-white focus:outline-none rounded-l-full"
+                >
+                  <FontAwesomeIcon icon={faChevronLeft} />
+                </button>
+              </div>
+            )}
+            {showRightButton && (
+              <div className="absolute top-0 right-0 h-full flex items-center bg-gradient-to-l from-gray-200 dark:from-gray-800 to-transparent">
+                <button
+                  onClick={() => {
+                    watchContainer.current?.scrollBy({
+                      left: watchContainer.current?.offsetWidth,
+                      behavior: "smooth",
+                    });
+                  }}
+                  className="px-4 h-full flex items-center justify-center text-gray-500 dark:text-gray-300 hover:text-gray-700 dark:hover:text-white focus:outline-none rounded-l-full"
+                >
+                  <FontAwesomeIcon icon={faChevronRight} />
+                </button>
+              </div>
+            )}
+          </>
+        ) : (
+          <div className="flex items-center justify-center w-full h-[4rem] bg-gray-100 dark:bg-gray-800 rounded-md">
+            <p className="text-gray-500 dark:text-gray-300">
+              Você ainda não assistiu nenhum vídeo
+            </p>
+          </div>
+        )}
       </div>
     </>
   );
